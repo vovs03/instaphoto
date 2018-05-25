f7b5df159a99853e0180f40ce0109fd3c98a10a7
Vladimir Pavlychev <vladimir.pavlychev@OsArts.ru>
2018-05-25 18:47:37 +0300

    Refactoring & Adding 2 buttons

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,6 +5,4 @@ class PagesController < ApplicationController
   def about
   end
 
-  def root
-  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,5 @@ Rails.application.routes.draw do
   get 'pages/landing'
   get 'pages/about'
   
-
-
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
