a8a9fe736865909cd00602fbf800bacb042d6c81
Vladimir Pavlychev <vladimir.pavlychev@OsArts.ru>
2018-05-27 07:35:15 +0300

    Adding Design

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
 
   get 'pages/landing'
   get 'pages/about'
+  get 'pages/registration'
+  get 'pages/my-account'
+
   
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
