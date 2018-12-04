e30a91dcc77867a0ef25280f861c594ba4199a07
Vladimir Pavlychev <vladimir.pavlychev@osarts.ru>
2018-12-04 16:44:44 +0300

    Upd :recycle: set bundle install before + echo decriptions + open page in Firefox

--- start-project-local.sh
+++ start-project-local.sh
@@ -1,9 +1,28 @@
-#!/usr/bin/bash
+#!/usr/bin/sh
 # 2018-12-04 15:57
-# 
+# Vladimir Pavlychev
 
+
+echo "Using bundle install"
+sleep 3
+
+bundle install
+sleep 3
+
+
+echo "bundle install --without production"
+sleep 3
 bundle install --without production
+sleep 3
 
+echo "bin/rails db:migrate RAILS_ENV=development"
+sleep 3
 bin/rails db:migrate RAILS_ENV=development
+sleep 3
+
+echo "Start Rails, opening ... Firefox "
+/usr/bin/firefox -new-tab "http://0.0.0.0:3000/"
+sleep 3
+echo "Press F5 for refresh opened page in browser"
 
 bin/rails s
