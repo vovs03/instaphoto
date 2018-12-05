b43a5a756d05957a855c98400c2835868a9ac702
Vladimir Pavlychev <vladimir.pavlychev@osarts.ru>
2018-12-05 12:25:04 +0300

    Fix: :bug: #15 if set  - output: Not found

--- set-alias.sh
+++ set-alias.sh
@@ -5,7 +5,7 @@
 #`alias shs='sh s.sh'` to ~/.bashrc & ~/.zshrc'
 
 clear
-echo "Set alias `shs` to ~/.bashrc "
+echo "Set alias -shs- to ~/.bashrc "
 
 echo "# Settings alias for project https://github.com/KeyJoo/instaphoto" >> ~/.bashrc 
 echo "# Run it only in specifically in ~/code/github/instaphoto " >> ~/.bashrc 
@@ -13,9 +13,9 @@ echo "alias shs='sh s.sh'" >> ~/.bashrc
 sleep 1.5
 
 echo ""
-echo "Set alias `shs` to ~/.zshrc "
+echo "Set alias -shs- to ~/.zshrc "
 echo "# Settings alias for project https://github.com/KeyJoo/instaphoto" >> ~/.zshrc 
-echo "# Run it only in specifically in ~/code/github/instaphoto " >> ~/.bashrc 
+echo "# Run it only in specifically in ~/code/github/instaphoto " >> ~/.zshrc 
 echo "alias shs='sh s.sh'" >> ~/.zshrc
 sleep 1.5
 
